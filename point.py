adb356bde9aca6d628ec7e582c44e31ff478d934
Ethan <ealook@calpoly.edu>
2015-04-08 12:44:46 -0700

    Changed WorldView to methods.

--- point.py
+++ point.py
@@ -2,3 +2,7 @@ class Point:
    def __init__(self, x, y):
       self.x = x
       self.y = y
+
+   def adjacent_to(self, other):
+	   return ((self.x == other.x and abs(self.y - other.y) == 1) or
+		      (self.y == other.y and abs(self.x - other.x) == 1))
