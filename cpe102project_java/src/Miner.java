0eb4d0b72ce4263a6b68ffa9c60f2216418dcecd
Ethan <ealook@calpoly.edu>
2015-05-03 19:20:01 -0700

    Added functionality to Miner class.

--- cpe102project_java/src/Miner.java
+++ cpe102project_java/src/Miner.java
@@ -34,4 +34,64 @@ public class Miner extends Mover {
         return new_entity;
     }
 
+    public boolean miner_to_ore(WorldModel world, Ore ore) {
+        Point entity_pt = this.getPosition();
+        if (ore == null) {
+            return false;
+        }
+        Point ore_pt = ore.getPosition();
+        if (entity_pt.adjacent_to(ore_pt)) {
+            this.set_resource_count(1 + this.get_resource_count());
+            //actions.remove_entity(world, ore);
+            world.remove_entity(ore);
+            return true;
+        } else {
+            Point new_pt = next_position(world, entity_pt, ore_pt);
+            world.move_entity(this, new_pt);
+            return false;
+        }
+    }
+
+    public boolean miner_to_smith(WorldModel world, Blacksmith smith) {
+        Point entity_pt = this.getPosition();
+        if (smith == null) {
+            return false;
+        }
+        Point smith_pt = smith.getPosition();
+        if (entity_pt.adjacent_to(smith_pt)) {
+            smith.set_resource_count(smith.get_resource_count() + this.get_resource_count());
+            this.set_resource_count(0);
+            return true;
+        } else {
+            Point new_pt = next_position(world, entity_pt, smith_pt);
+            world.move_entity(this, new_pt);
+            return false;
+        }
+    }
+
+    public static Point next_position(WorldModel world, Point entity_pt, Point dest_pt) {
+        int horiz = sign(dest_pt.getX() - entity_pt.getX());
+        Point new_pt = new Point(entity_pt.getX() + horiz, entity_pt.getY());
+
+        if (horiz == 0 || world.is_occupied(new_pt)) {
+            int vert = sign(dest_pt.getY() - entity_pt.getY());
+            new_pt = new Point(entity_pt.getX(), entity_pt.getY() + vert);
+
+            if (vert == 0 || world.is_occupied(new_pt)) {
+                new_pt = entity_pt;
+            }
+        }
+
+        return  new_pt;
+    }
+
+    public static int sign(int x) {
+        if (x < 0) {
+            return -1;
+        } else if (x > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
